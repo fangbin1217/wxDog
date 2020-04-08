f801770720e44ad1aacc085f27f93e4fb2b8d61c
fangbin <jsonp@sina.com>
2020-04-08 18:21:39 +0800

    a

--- pages/dog/index.js
+++ pages/dog/index.js
@@ -12,7 +12,10 @@ Page({
     hatImage: '../../images/hat_small0408.png',
     wjImage: '../../images/wj_small0408.png',
     wanImage: '../../images/wan_small0408.png',
-    jsImage: '../../images/js_small0408.png'
+    jsImage: '../../images/js_small0408.png',
+    bqImage1: '../../images/bq1.png',
+    bqImage2: '../../images/bq2.png',
+    bqImage3: '../../images/bq3.png',
   },
   onLoad: function (options) {
 
@@ -42,10 +45,10 @@ Page({
 
   indexInit: function() {
     var daojuList = [
-      {daojuId: 1, title: '帽子道具卡', image: app.globalData.imgHost + 'index/hat_small0408.png', counts: 1},
-      {daojuId: 2, title: '帽子道具卡', image: app.globalData.imgHost + 'index/wj_small0408.png', counts: 0},
-      {daojuId: 3, title: '帽子道具卡', image: app.globalData.imgHost + 'index/wan_small0408.png', counts: 0},
-      {daojuId: 4, title: '帽子道具卡', image: app.globalData.imgHost + 'index/js_small0408.png', counts: 0}
+      {daojuId: 1, title: '帽子道具卡', image: app.globalData.imgHost + 'index/hat_small0408.png', counts: 1, isWear: false},
+      {daojuId: 2, title: '围巾道具卡', image: app.globalData.imgHost + 'index/wj_small0408.png', counts: 0, isWear: false},
+      {daojuId: 3, title: '金碗道具卡', image: app.globalData.imgHost + 'index/wan_small0408.png', counts: 1, isWear: true},
+      {daojuId: 4, title: '加速道具卡', image: app.globalData.imgHost + 'index/js_small0408.png', counts: 0, isWear: false}
     ];
 
     var cateList = [
@@ -54,7 +57,8 @@ Page({
 
     this.setData({
       daojuList: daojuList,
-      cateList: cateList
+      cateList: cateList,
+      dogXqValue:0
     });
     
   }
